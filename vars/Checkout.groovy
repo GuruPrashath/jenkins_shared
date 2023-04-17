8dc2af676dbfae4fa7e7ff1c6e9b87bfa7c41961
Guru Prashath M <guruprashath005@gmail.com>
2023-04-17 08:35:56 +0000

    Update Checkout.groovy

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,4 +1,5 @@
 def call (def giturl,def branch){
+    echo "hurray!!!"
 checkout([$class: 'GitSCM', branches: [[name: branch ]], extension: [], userRemoteConfigs: [
 [credentialsId: 'github', url:giturl]
 ]])
